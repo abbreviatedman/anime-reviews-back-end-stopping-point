9d8490327a310c6fc087de7570ea871f655d015c
Colin Jaffe <balloonasaurus@gmail.com>
2022-02-13 08:40:34 -0800

    Fixes minor issue.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -5,8 +5,7 @@ require("dotenv").config();
 
 // this is the object we are passing to pr-promise so we can connect to the proper db
 const cn = {
-  DATABASE_URL:
-    "postgres://olwhjzrzznzcex:37045a5593cf766391a4f77bb50dd0109017dba767ebdabe8ed7118feffda146@ec2-54-159-244-207.compute-1.amazonaws.com:5432/d8nqginudrrpiq",
+  DATABASE_URL: process.env.DATABASE_URL,
 };
 
 //  database is now our db i.e anime_dev
