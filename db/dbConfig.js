7df4b851baf9cec36bc4b53f2b35568162fad240
Colin Jaffe <balloonasaurus@gmail.com>
2022-02-13 09:15:03 -0800

    Tries previous style of config with SSL.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -3,6 +3,9 @@ const pgp = require("pg-promise")();
 // allows us to access our .env variables
 require("dotenv").config();
 
+const ssl = { rejectUnauthorized: false };
+const max = 30;
+
 // this is the object we are passing to pr-promise so we can connect to the proper db
 const cn = {
   host: process.env.PG_HOST,
@@ -10,14 +13,16 @@ const cn = {
   database: process.env.PG_DATABASE,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
+  max,
+  ssl,
 };
 
 const config = {
   connectionString: process.env.DATABASE_URL,
-  max: 30,
-  ssl: { rejectUnauthorized: false },
+  max,
+  ssl,
 };
 
-const database = pgp(config);
+const database = pgp(cn);
 
 module.exports = database;
