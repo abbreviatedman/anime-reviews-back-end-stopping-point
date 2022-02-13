ea6ec97fe263e360655af662126779d2ac00308e
Colin Jaffe <balloonasaurus@gmail.com>
2022-02-13 07:26:27 -0800

    Uses password.

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -9,6 +9,7 @@ const cn = {
   port: process.env.PG_PORT,
   database: process.env.PG_DATABASE,
   user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
 };
 
 //  database is now our db i.e anime_dev
